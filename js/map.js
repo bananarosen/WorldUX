9ec2f92e3f1e6e74db127bf0d6616192eb14e13f
Hannah Rosen <hannahfayerosen@gmail.com>
2016-12-03 13:40:24 -0500

    Assignment 5 version

---
+++ js/map.js
@@ -0,0 +1,5 @@
+var map = new Datamap({element: document.getElementById('container')});
+
+/*pop up with the information about internet speed, percentage of population with internet access and smartphone access, and share of world internet users. Plus more button that leads to individual country pages. */
+
+/*Sort by drop down menu with other options to re-create the data visualization on the map based on the 3 different data sets*/
